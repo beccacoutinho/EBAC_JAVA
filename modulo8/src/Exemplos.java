6dfdc2d2d01d5044273e741fba754d747fc46d48
NaimCaio <beckinhacoutinho@gmail.com>
2022-10-26 11:38:01 -0300

    modulo 8: operadores

---
+++ modulo8/src/Exemplos.java
@@ -0,0 +1,34 @@
+public class Exemplos {
+
+    private int codigo=1;
+    private long codigoMaior=1284050;
+    private double valorDecimal1=10.1;
+    private boolean status = false;
+    private String texto = "aksodkaosd54";
+
+    private float valorDecimal = 10.0f;
+    private short shor;
+    private byte bi;
+
+    public Exemplos(int val){
+        this.codigo = val;
+
+    }
+
+    public String retornarTexto(){
+        this.codigo = 0;
+        return "sasadada";
+    }
+
+    public int retornarInteiro(){
+        int val =10;
+        String texto ="Texto";
+        this.texto = null;
+        return val;
+    }
+
+    public long retornarLong(long num){
+        this.texto="sodkfsd";
+        return num;
+    }
+}
---
+++ modulo8/src/TarefaCalculoMedia/Exercicio.java
@@ -0,0 +1,15 @@
+package TarefaCalculoMedia;
+
+public class Exercicio {
+    public static void main(String args[]) {
+        int nota1 = 7;
+        int nota2 = 8;
+        int nota3 = 9;
+        int nota4 = 5;
+
+        int media = (nota1 + nota2 + nota3 + nota4) /4;
+
+        System.out.println("O aluno tirou as seguintes notas nas provas: 7, 8, 9, 5. ");
+        System.out.println("Ele ficou com a nota " + media + " como media.");
+    }
+}
